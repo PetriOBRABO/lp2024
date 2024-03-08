cbdfe10189037578b2ba6e29cfd93fa461f5f27c
PetriOBRABO <158631086+PetriOBRABO@users.noreply.github.com>
2024-03-08 12:18:02 -0400

    Update lista01.c

--- lista01.c
+++ lista01.c
@@ -344,6 +344,50 @@ void questao17() {
 
 //18. Faça um programa que leia um número inteiro entre 1 e 12 e escreva o mês correspondente. Caso o usuário digite umnúmero fora desse intervalo, deverá aparecer uma mensagem informando que não existe mês com este número. Utilize o switch para este problema.
 void questao18() {
+	int num;
+	printf("Digite um numero de 1 a 12");
+	scanf("%d",&num);
+
+	switch(num){
+		case 1: 
+		printf("Janeiro\n");
+		break;
+		case 2: 
+		printf("Fevereiro\n");
+		break;
+		case 3: 
+		printf("Marco\n");
+		break;
+		case 4: 
+		printf("Abril\n");
+		break;
+		case 5: 
+		printf("Maio\n");
+		break;
+		case 6: 
+		printf("Junho\n");
+		break;
+		case 7: 
+		printf("Julho\n");
+		break;
+		case 8: 
+		printf("Agosto\n");
+		break;
+		case 9: 
+		printf("Setembro\n");
+		break;
+		case 10: 
+		printf("Outubro\n");
+		break;
+		case 11: 
+		printf("Novembro\n");
+		break;
+		case 12: 
+		printf("Dezembro\n");
+		break;
+			default:
+			printf("Nao existe mes correspondente a este numero.");
+				}
 	
 }
 
@@ -352,6 +396,10 @@ void questao18() {
 //• Mostrar esses valores em ordem decrescente;
 //• Se a soma dos pontos for maior do que 100, imprimir a média aritmética entre eles, caso contrário, imprimir a mensagem "Equipe desclassificada".
 void questao19() {
+	int equipe1,equipe2,equipe3;
+
+	
+	
 	
 }
 
